ddcc24ad3678561582db15ad83945dfaec416aed
Gab <gabytab3009@gmail.com>
2024-01-11 17:59:56 -0300

    Gabrielle: comentando partes importantes da minha implementação

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,27 +1,34 @@
+// Seleciona os elementos do formulário de login e registro, bem como o botão de transição de cores
 var formSignin = document.querySelector("#signin");
 var formSignup = document.querySelector("#signup");
 var btnColor = document.querySelector(".btnColor");
 
+// Adiciona um ouvinte de eventos para o botão de login, alterando a posição dos formulários e do botão
 document.querySelector("#btnSignin").addEventListener("click", () => {
   formSignin.style.left = "25px";
   formSignup.style.left = "450px";
   btnColor.style.left = "0px";
 });
 
+// Adiciona um ouvinte de eventos para o botão de registro, alterando a posição dos formulários e do botão
 document.querySelector("#btnSignup").addEventListener("click", () => {
   formSignin.style.left = "-450px";
   formSignup.style.left = "25px";
   btnColor.style.left = "110px";
 });
 
+// Seleciona os elementos do formulário de registro e o campo de e-mail
 var signupForm = document.querySelector("#signup");
 var signupEmailInput = document.querySelector("#email");
 
+// Adiciona um ouvinte de eventos para o envio do formulário de registro
 signupForm.addEventListener("submit", function (event) {
+  // Chama a função para verificar se o e-mail já está cadastrado
   isEmailAlreadyRegistered(signupEmailInput.value, function (isRegistered) {
     if (isRegistered) {
+      // Exibe um alerta se o e-mail já estiver cadastrado e impede o envio do formulário
       alert("Este e-mail já está cadastrado. Por favor, escolha outro.");
-      event.preventDefault(); // Impede o envio do formulário se o e-mail já estiver cadastrado
+      event.preventDefault();
     }
   });
 });
